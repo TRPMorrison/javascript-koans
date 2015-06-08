ac272b853f581c9283526c8d88fc76f45bad3b75
Trini Morrison <TRPMorrison@gmail.com>
2015-06-08 17:13:04 -0400

    REMOVED EXTRA ERROR

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -68,7 +68,7 @@ describe("About Higher Order Functions", function () {
     var isEven = function(x) { return x % 2 === 0 };
 
     expect(_(onlyEven).any(isEven)).toBe[2, 4, 6];
-    expect(_(mixedBag).any(isEven)).toBe[2, 4, 5,, 6];
+    expect(_(mixedBag).any(isEven)).toBe[2, 4, 5, 6];
   });
 
   it("should use range to generate an array", function() {
